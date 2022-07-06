0dd3b29405ceae4cf63e2396b01387a0e84822cf
Tom <tombinnie@protonmail.com>
2022-07-06 20:37:51 +0100

    add events and ui with DOM manip

--- script.js
+++ script.js
@@ -17,24 +17,43 @@ const outcomes = {
 
 const computerPlay = () => guuPaaChoki[Math.floor(Math.random()*3)];
 
-const playRound = function (playerMove=prompt('Enter guu, paa, or choki:').toLowerCase(), computerMove=computerPlay()) {
-    while (guuPaaChoki.indexOf(playerMove) == -1) {
-        playerMove = prompt('Enter guu, paa, or choki:' );
-    };
+const playRound = function (playerMove, computerMove=computerPlay()) {
     return [playerMove, computerMove];
 };
 
-const game = function () {
-    let score = 0
-    for (let i = 1; i <= 5; i++) {
-        let outcome = playRound()
-        console.log(outcome[0], outcome[1])
+// const game = function () {
+//         console.log(outcome[0], outcome[1])
+//         if (outcome[0] === outcome[1]) {
+//             console.log('Draw!');
+//             i--;
+//         } else {
+//             score += outcomes[outcome[0]][outcome[1]]
+//         };
+//     }
+//     return (score > 2 ? 'You Win!' : 'You Lose :(');
+
+const draws = document.querySelector('.draws')
+const wins = document.querySelector('.wins')
+const losses = document.querySelector('.losses')
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let outcome = playRound(button.id);
         if (outcome[0] === outcome[1]) {
-            console.log('Draw!');
-            i--;
+            draws.textContent = Number(draws.textContent) + 1;
+        } else if (outcomes[outcome[0]][outcome[1]] === 0) {
+            losses.textContent = Number(losses.textContent) + 1
         } else {
-            score += outcomes[outcome[0]][outcome[1]]
-        };
-    }
-    return (score > 2 ? 'You Win!' : 'You Lose :(');
-}
+            wins.textContent = Number(wins.textContent) + 1
+            
+        }
+        if (wins.textContent === '5') {
+            alert('You win!')
+        }
+        if (losses.textContent === '5') {
+            alert('You lose :(')
+        }
+        });
+        });
